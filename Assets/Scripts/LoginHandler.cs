4ed088f3ac7131882c8f5979adbd918f8274212b
CADIndie <84008186+CADIndie@users.noreply.github.com>
2022-10-24 23:54:23 -0400

    fuuuuuuuu.....

--- Assets/Scripts/LoginHandler.cs
+++ Assets/Scripts/LoginHandler.cs
@@ -6,9 +6,14 @@ public class LoginHandler : MonoBehaviour
 {
     public string ACCESS_TOKEN;
     public string OAUTH_CODE;
+    AndroidJavaClass unityClass;
+    private AndroidJavaObject unityActivity;
 
+    
     public void SwitchTask()
     {
-        
+        unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        unityActivity = unityClass.GetStatic<AndroidJavaObject>("currentActivity");
+        PluginInit._pluginInstance.CallStatic("activity", unityActivity);
     }
 }
--- Assets/Scripts/PluginInit.cs
+++ Assets/Scripts/PluginInit.cs
@@ -6,7 +6,7 @@ public class PluginInit : MonoBehaviour
 {
     AndroidJavaClass unityClass;
     private AndroidJavaObject unityActivity;
-    public AndroidJavaObject _pluginInstance;
+    public static AndroidJavaObject _pluginInstance;
     
     // Start is called before the first frame update
     void Start()
@@ -16,8 +16,6 @@ public class PluginInit : MonoBehaviour
 
     void InitializePlugin(string pluginName)
     {
-        unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        unityActivity = unityClass.GetStatic<AndroidJavaObject>("currentActivity");
         _pluginInstance = new AndroidJavaObject(pluginName);
         if (_pluginInstance == null)
         {
@@ -34,10 +32,5 @@ public class PluginInit : MonoBehaviour
             var result = _pluginInstance.Call<int>("Add", 5, 6);
             Debug.Log("Add Result from Unity: " +result);
         }
-    }
-
-    public void OAuthTokan()
-    {
-        
     }
 }
