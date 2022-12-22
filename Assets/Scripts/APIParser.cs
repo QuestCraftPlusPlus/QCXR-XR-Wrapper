e4e81c7cb4c2139605d98b044398e5882b59c23d
CADIndie <84008186+CADIndie@users.noreply.github.com>
2022-12-22 14:15:13 -0500

    please be no corrupt

---
+++ Assets/Scripts/APIParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+[System.Serializable]
+
+public class APIParser
+{
+    public string results;
+    public string title;
+    public string id;
+    public string number;
+    public string url;
+    public string image;
+    public string sources;
+    public string headers;
+    public string version;
+
+}
--- Assets/Scripts/OTAHandler.cs
+++ Assets/Scripts/OTAHandler.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using OVRSimpleJSON;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class OTAHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /*public GameObject releaseInfoJson;
+    
     void Start()
     {
-        
+        APIParser ri = OTAHandler.GetLatestVersion();
+
+        /*if (ri.version != currentVersion)
+        {
+            new DownloadHandlerFile("test");
+        }#1#
     }
 
-    // Update is called once per frame
-    void Update()
+    public static APIParser GetLatestVersion()
     {
-        
-    }
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://apihere.com/release_info/");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string json = reader.ReadToEnd();
+        Debug.Log(json);
+
+        return JsonUtility.FromJson<APIParser>(json);
+    }*/
 }
