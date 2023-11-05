c5f7890089c1f4f607b8c317ac8da90c1773365e
The Judge <colepatrickroberts@gmail.com>
2023-11-05 15:22:29 -0800

    cleanup

--- Assets/Scripts/JNIStorage.cs
+++ Assets/Scripts/JNIStorage.cs
@@ -16,18 +16,12 @@ public class JNIStorage : MonoBehaviour
 
     private void Start()
     {
-		if(!Permission.HasUserAuthorizedPermission("org.khronos.openxr.permission.OPENXR_SYSTEM")) {
-			Permission.RequestUserPermission("org.khronos.openxr.permission.OPENXR_SYSTEM");
-		}
-		if(!Permission.HasUserAuthorizedPermission("android.permission.VIBRATE")) {
-			Permission.RequestUserPermission("android.permission.VIBRATE");
-		}
         Microphone.Start(Microphone.devices[0], true, 1, 44100);
         apiClass = new AndroidJavaClass("pojlib.api.API_V1");
         AndroidJavaClass constants = new AndroidJavaClass("pojlib.util.Constants");
         home = constants.GetStatic<string>("MC_DIR");
         UpdateInstances();
-	apiClass.SetStatic("model", OpenXRFeatureSystemInfo.GetHeadsetName());
+		apiClass.SetStatic("model", OpenXRFeatureSystemInfo.GetHeadsetName());
     }
 
     public static void UpdateInstances()
