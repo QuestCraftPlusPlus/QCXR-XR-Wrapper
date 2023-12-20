b27b7e6c97487f44c0cfaac22d511d7129bd5526
Florian Reintgen <85225228+HowToCodeDE@users.noreply.github.com>
2023-12-21 00:26:50 +0100

    Update LoginText.cs
    Parses the authentication code from Microsoft's message

--- Assets/Scripts/LoginText.cs
+++ Assets/Scripts/LoginText.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 public class LoginText : MonoBehaviour
 {
+    string code = "";
+
     void Update()
     {
         GetComponent<TextMeshProUGUI>().text = JNIStorage.apiClass.GetStatic<string>("msaMessage");
         if(GetComponent<TextMeshProUGUI>().text != "")
         {
-            GetComponent<TextMeshProUGUI>().text  = GetComponent<TextMeshProUGUI>().text + "Complete then you will be logged in.";
+	    string message = JNIStorage.apiClass.GetStatic<string>("msaMessage");
+
+            Match match = Regex.Match(message, "enter the code (\\w+)", RegexOptions.IgnoreCase);
+
+            if (match.Success)
+            {
+                code = match.Groups[1].Value;
+	 	        GetComponent<TextMeshProUGUI>().text  = GetComponent<TextMeshProUGUI>().text + "Complete to continue. To use on device authentication, please press 'Sign In' again.";
+            }
+            else 
+	        {
+		        code = "";
+		        GetComponent<TextMeshProUGUI>().text  = GetComponent<TextMeshProUGUI>().text + "Complete to continue.";
+            }
         }
+    }
+
+    public string getMsaCode(){
+    	return code;
     }
 }
