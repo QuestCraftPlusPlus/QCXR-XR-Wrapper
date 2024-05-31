03eae4926fe024bac6e2fa43e8b3bc8ae449205e
tr1ngledev <steamkyzagames2@gmail.com>
2024-05-31 14:05:54 +0800

    more fun stuff

--- Assets/Scripts/DownloadTextManager.cs
+++ Assets/Scripts/DownloadTextManager.cs
@@ -15,20 +15,22 @@ public class DownloadTextManager : MonoBehaviour
 	void FixedUpdate()
 	{
 		string currentFile = JNIStorage.apiClass.GetStatic<string>("currentDownload");
-		double mbDownloaded = Math.Round(JNIStorage.apiClass.GetStatic<double>("downloadStatus"), 3);
+		double mbDownloaded = Math.Round(JNIStorage.apiClass.GetStatic<double>("downloadStatus"), 2);
+		double mbTotal = Math.Round(JNIStorage.apiClass.GetStatic<double>("downloadStatusTotal"), 2);
 
 		int hasDownloaded = JNIStorage.apiClass.GetStatic<int>("hasDownloaded");
 		int isDownloading = JNIStorage.apiClass.GetStatic<int>("isDownloading");
+		string downloadStep = JNIStorage.apiClass.GetStatic<string>("downloadStep");
 
 		if (!string.IsNullOrWhiteSpace(currentFile))
-			text.text = $"Downloading {currentFile}: {mbDownloaded} MB";
+			text.text = $"Downloading {currentFile}: {mbDownloaded}MB/{mbTotal}MB";
 		else 
 			text.text = "";
 
 		if(isDownloading > 0)
 		{
 			JNIStorage.instance.progressBar.fillAmount = Mathf.Lerp(JNIStorage.instance.progressBar.fillAmount, (float)hasDownloaded / (float)isDownloading, 0.1f);
-			JNIStorage.instance.progressBarText.text = $"{hasDownloaded}/{isDownloading}";
+			JNIStorage.instance.progressBarText.text = $"{hasDownloaded}/{isDownloading} [{downloadStep}]";
 		}
 		else
 		{
